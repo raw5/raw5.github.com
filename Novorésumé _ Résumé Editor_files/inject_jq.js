0bd08a51d5f0f5e66252694876cfc5debdd2ce04
raw <vipin.itm@gmail.com>
2016-11-04 14:02:37 +0530

    add nova resume

---
+++ Novorésumé _ Résumé Editor_files/inject_jq.js
@@ -0,0 +1,2 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Error><Code>AccessDenied</Code><Message>Access Denied</Message><RequestId>656C027EE29A46B2</RequestId><HostId>+a4ayXpyYvnHQ2Y4sQ9BeRBLS4Iuc/g5tvVt7Pb0GHyamuDejqVpGTUeRMadbNQVfFCDN+wxjFc=</HostId></Error>
